6c79c33de957f29a4baff9e4ad7816feee23cc83
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-07 20:53:36 -0300

    Metadata in titles

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -2,6 +2,7 @@ import { api } from "@/data/api";
 import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
+import { Metadata } from 'next'
 
 
 async function getFeaturedProducts(): Promise<Product[]> {
@@ -14,6 +15,10 @@ async function getFeaturedProducts(): Promise<Product[]> {
   const products = await response.json()
 
   return products
+}
+
+export const metadata: Metadata = {
+  title: 'Home',
 }
 
 export default async function Home() {
