f4bcb92dc823b628e3f4c96d338ee2804a797b65
Enzo Salomão <enzosadourado@hotmail.com>
2024-07-07 20:12:31 -0300

    pageOfProducts

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -2,6 +2,9 @@ import data from '../data.json'
 
 
 export async function GET() {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+
+
     const featuredProducts = data.products.filter(product => product.featured)
 
 
